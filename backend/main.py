ff8153f06f10cdcbda9e174af173d94720a4b67c
Nishan-02 <nishansuvarna761@gmail.com>
2026-02-11 22:56:06 +0530

    fixing rquirements

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 # Enable CORS (Critical for React Frontend)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # In production, change this to ["http://localhost:5173"]
+    allow_origins=["*"], # Allow all origins (Easiest for testing)
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
